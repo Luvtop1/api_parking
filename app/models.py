4659bc219870009e6d3ad7fe460ddfc903630957
Yurii Leshcenko <kapacb.1997@mail.ru>
2025-07-26 18:43:33 +0300

    Fix flake8 issues in models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from typing import List, Optional
+from typing import List  # noqa: F401
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -27,7 +26,9 @@ class Parking(db.Model):  # type: ignore[name-defined]
     address = db.Column(db.String(100), nullable=False)  # type: ignore[attr-defined]
     opened = db.Column(db.Boolean)  # type: ignore[attr-defined]
     count_places = db.Column(db.Integer, nullable=False)  # type: ignore[attr-defined]
-    count_available_places = db.Column(db.Integer, nullable=False)  # type: ignore[attr-defined]
+    count_available_places = db.Column(
+        db.Integer, nullable=False
+    )  # type: ignore[attr-defined]
 
     clients: List["ClientParking"] = db.relationship(  # type: ignore[attr-defined]
         "ClientParking", backref="parking", lazy=True
@@ -38,11 +39,17 @@ class ClientParking(db.Model):  # type: ignore[name-defined]
     __tablename__ = "client_parking"
 
     id = db.Column(db.Integer, primary_key=True)  # type: ignore[attr-defined]
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"))  # type: ignore[attr-defined]
-    parking_id = db.Column(db.Integer, db.ForeignKey("parking.id"))  # type: ignore[attr-defined]
+    client_id = db.Column(  # type: ignore[attr-defined]
+        db.Integer, db.ForeignKey("client.id")
+    )
+    parking_id = db.Column(  # type: ignore[attr-defined]
+        db.Integer, db.ForeignKey("parking.id")
+    )
     time_in = db.Column(db.DateTime, nullable=True)  # type: ignore[attr-defined]
     time_out = db.Column(db.DateTime, nullable=True)  # type: ignore[attr-defined]
 
     __table_args__ = (
-        db.UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),  # type: ignore[attr-defined]
+        db.UniqueConstraint(  # type: ignore[attr-defined]
+            "client_id", "parking_id", name="unique_client_parking"
+        ),
     )
