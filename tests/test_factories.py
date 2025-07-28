b004112cfe65bf86d773871d6fdce3352d4efd31
Luvtop1 <kapacb.1997@mail.ru>
2025-07-28 11:08:05 +0300

    Update test_factories.py

---
+++ tests/test_factories.py
@@ -0,0 +1,20 @@
+from factories import ClientFactory, ParkingFactory
+
+
+def test_create_client_with_factory(db_session):
+    ClientFactory._meta.sqlalchemy_session = db_session
+    client = ClientFactory()
+    assert client.id is not None
+    assert client.name is not None
+    assert client.surname is not None
+    assert isinstance(client.credit_card, (str, type(None)))
+
+
+def test_create_parking_with_factory(db_session):
+    ParkingFactory._meta.sqlalchemy_session = db_session
+    parking = ParkingFactory()
+    assert parking.id is not None
+    assert parking.address is not None
+    assert isinstance(parking.opened, bool)
+    assert parking.count_places > 0
+    assert parking.count_available_places == parking.count_places
